70e9c75ed5c889eb80fa658a1ea981db76e94c30
Arseniy <arseniyc1306@gmail.com>
2022-05-27 23:13:55 +0300

    Making UI

--- app/src/main/java/com/arsenijjke/favquotes/FirstFragment.kt
+++ app/src/main/java/com/arsenijjke/favquotes/FirstFragment.kt
@@ -1,12 +1,16 @@
 package com.arsenijjke.favquotes
 
+import android.media.MediaRouter
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.arsenijjke.favquotes.databinding.FragmentFirstBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,6 +34,34 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
                 binding.quoteBody.text = quote.value?.quote?.body
             })
         }
+    }
+
+    fun onSwipe() {
+        val swipeGesture = object : SwipeGesture() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                when(direction) {
+
+                    ItemTouchHelper.UP -> {
+                        toQuoteInfo()
+                    }
+
+                    ItemTouchHelper.RIGHT -> {
+                        makeFavourite()
+                    }
+                }
+
+                super.onSwiped(viewHolder, direction)
+            }
+        }
+    }
+
+    private fun toQuoteInfo() {
+        //TODO("By swiping up, move to fragment with info about this quote")
+    }
+
+    private fun makeFavourite() {
+        //TODO("By swiping right, add quote to favourites")
     }
 
     private fun viewModel(): MainViewModel {
